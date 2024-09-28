2d060f9626a07f260975cbb5a3d74662cc13831d
CursedAki <je.landaeta@duocuc.cl>
2024-09-27 23:56:53 -0300

    SeccionServicios

--- Proyecto/web-lexy/src/Screens/Services.js
+++ Proyecto/web-lexy/src/Screens/Services.js
@@ -1,8 +1,23 @@
 import React from 'react'
+import styles from "./Services.module.css"; 
+
 
 const Services = () => {
   return (
-    <div>Servicios</div>
+    (<div className={styles.services}>
+      <p>
+        Nuestros Servicios
+      </p>
+      <div>
+        <img className={styles.webImage} src={require("../Assets/ImgServicios.jpg")} alt=''/>
+        <img className={styles.webImage} src={require("../Assets/ImgServicios2.jpg")} alt=''/>
+        <img className={styles.webImage} src={require("../Assets/ImgServicios3.jpg")} alt=''/>
+        <img className={styles.webImage} src={require("../Assets/ImgServicios4.jpg")} alt=''/>
+
+
+      </div>
+      
+    </div>)
   )
 }
 
